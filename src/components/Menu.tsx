bdce7104c3aad519915344cee4fa5c1f6de94b38
vitalik.nozhenko@gmail.com <109283146+VitalikN@users.noreply.github.com>
2024-01-29 19:49:45 +0200

    contacts create

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -7,37 +7,45 @@ import Image from "next/image";
 const Menu = () => {
   return (
     <section className={styles.menu__section}>
-      <div className={`${styles.container} ${styles.menu__flex}`}>
-        <Link href="/ball" className={`${styles.navigation__link} `}>
-          <Image
-            className={styles.menu__img}
-            src="/ball.png"
-            alt="ball"
-            width="200"
-            height="100"
-            priority={true}
-          />
-        </Link>
-        <Link href="/cubes" className={`${styles.navigation__link} `}>
-          <Image
-            className={styles.menu__img}
-            src="/cubes.png"
-            alt="cubes"
-            width="150"
-            height="100"
-            priority={true}
-          />
-        </Link>
-        <Link href="/contacts" className={`${styles.navigation__link} `}>
-          <Image
-            className={styles.menu__img}
-            src="/contacts.png"
-            alt="contacts"
-            width="200"
-            height="100"
-            priority={true}
-          />
-        </Link>
+      <div className={`${styles.container} `}>
+        <ul className={styles.menu__flex}>
+          <li className={styles.list__item}>
+            <Link href="/ball" className={`${styles.navigation__link} `}>
+              <Image
+                className={styles.menu__img}
+                src="/ball.png"
+                alt="ball"
+                width="200"
+                height="200"
+                priority={true}
+              />
+            </Link>
+          </li>
+          <li className={styles.list__item}>
+            <Link href="/contacts" className={`${styles.navigation__link} `}>
+              <Image
+                className={styles.menu__img}
+                src="/contacts.png"
+                alt="contacts"
+                width="200"
+                height="200"
+                priority={true}
+              />
+            </Link>
+          </li>
+          <li className={styles.list__item}>
+            <Link href="/cubes" className={`${styles.navigation__link} `}>
+              <Image
+                className={styles.menu__img}
+                src="/cubes.png"
+                alt="cubes"
+                width="200"
+                height="200"
+                priority={true}
+              />
+            </Link>
+          </li>
+        </ul>
       </div>
     </section>
   );
