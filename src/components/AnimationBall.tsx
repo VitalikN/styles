04a12b4f3d1223077a90375f71c72198d3c133af
vitalik.nozhenko@gmail.com <109283146+VitalikN@users.noreply.github.com>
2024-01-30 01:08:09 +0200

    fix menu

--- src/components/AnimationBall.tsx
+++ src/components/AnimationBall.tsx
@@ -4,8 +4,8 @@ import styles from "../sass/layouts/animationBall.module.scss";
 
 const AnimationBall = () => {
   return (
-    <section>
-      <div className={styles.container}>
+    <section className={styles.ball__section}>
+      <div className={`${styles.container} ${styles.animationBall__container}`}>
         <div className={styles.box}>
           <div className={styles.ball}></div>
           <div className={styles.ball}></div>
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,48 +1,60 @@
 "use client";
 import Link from "next/link";
 
+import { AiOutlineMail } from "react-icons/ai";
 import styles from "../sass/layouts/menu.module.scss";
 import Image from "next/image";
+
+import { FaSearch, FaComments } from "react-icons/fa";
 
 const Menu = () => {
   return (
     <section className={styles.menu__section}>
       <div className={`${styles.container} `}>
         <ul className={styles.menu__flex}>
-          <li className={styles.list__item}>
+          <li className={`${styles.list__item} ${styles.list__item__ball}`}>
             <Link href="/ball" className={`${styles.navigation__link} `}>
-              <Image
-                className={styles.menu__img}
-                src="/ball.png"
-                alt="ball"
-                width="200"
-                height="200"
-                priority={true}
-              />
+              <div className={`${styles.box} ${styles.box__ball__menu}`}>
+                <div className={`${styles.ball} ${styles.ball__menu}`}></div>
+
+                <div
+                  className={`${styles.shadow} ${styles.shadow__menu}`}
+                ></div>
+              </div>
             </Link>
           </li>
           <li className={styles.list__item}>
             <Link href="/contacts" className={`${styles.navigation__link} `}>
-              <Image
-                className={styles.menu__img}
-                src="/contacts.png"
-                alt="contacts"
-                width="200"
-                height="200"
-                priority={true}
-              />
+              <div className={styles.contacts__list__item}>
+                <div className={styles.contacts__list__link}>
+                  <AiOutlineMail className={styles.contacts__list__icon} />
+                </div>
+              </div>
             </Link>
           </li>
-          <li className={styles.list__item}>
-            <Link href="/cubes" className={`${styles.navigation__link} `}>
-              <Image
-                className={styles.menu__img}
-                src="/cubes.png"
-                alt="cubes"
-                width="200"
-                height="200"
-                priority={true}
-              />
+          <li className={`${styles.list__item} ${styles.cubes__item}`}>
+            <Link
+              href="/cubes"
+              className={`${styles.navigation__link} ${styles.cubes__navigation__link} `}
+            >
+              <div className={styles.cubes__list}>
+                <div className={styles.cubes__list__item}>
+                  <div className={`${styles.cubes__link} `}>
+                    <span className={styles.cubes__list__icon}>
+                      <FaComments />
+                    </span>
+                    <h3 className={styles.cubes__title}>title1</h3>
+                  </div>
+                </div>
+                <div className={styles.cubes__list__item}>
+                  <div className={`${styles.cubes__link} `}>
+                    <span className={styles.cubes__list__icon}>
+                      <FaSearch />
+                    </span>
+                    <h3 className={styles.cubes__title}>title2</h3>
+                  </div>
+                </div>
+              </div>
             </Link>
           </li>
         </ul>
